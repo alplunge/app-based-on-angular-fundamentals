e0cecee87614eff958b67156b861324ae94fc09a
Al Plunge <al.plunge@gmail.com>
2019-06-13 22:00:32 +0300

    two way data binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,25 +5,25 @@ import {Component} from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <h1 [innerHTML] = title></h1>
-      <div>
-      {{random + randomTwo}}
-      </div>
-      <div>
-      {{isHappy ? ':)' : ':('}}
-      </div>
-      <input type="text" [value]="name">
+    <button (click)="handleClick()">change to my name</button>
+      <input
+      (input)="handleInput($event)" 
+        type="text" 
+        [value]="name"
+      (blur)="handleBlur($event)">
       <div>{{name}}</div>
     </div>
   `
 })
 export class AppComponent {
-  title: string;
   name: string = 'A course!';
-  age: number = 3;
-  random: number = 2;
-  randomTwo: boolean = true;
-  constructor(){
-    this.title = 'Ultimate Plunge';
+  handleClick(){
+    this.name = 'Ultimate Plunge';
+  };
+  handleInput(event: any){
+    this.name = event.target.value;
+  }
+  handleBlur(event: any){
+    console.log(event);
   }
 }
