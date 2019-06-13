4e00da488f2e144a3f4e69de098fa98e28aa2d5f
Al Plunge <al.plunge@gmail.com>
2019-06-13 21:41:18 +0300

    playground

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,12 +5,24 @@ import {Component} from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      {{title}}
+      <h1 [innerHTML] = title></h1>
+      <div>
+      {{random + randomTwo}}
+      </div>
+      <div>
+      {{isHappy ? ':)' : ':('}}
+      </div>
+      <input type="text" [value]="name">
+      <div>{{name}}</div>
     </div>
   `
 })
 export class AppComponent {
   title: string;
+  name: string = 'A course!';
+  age: number = 3;
+  random: number = 2;
+  randomTwo: boolean = true;
   constructor(){
     this.title = 'Ultimate Plunge';
   }
